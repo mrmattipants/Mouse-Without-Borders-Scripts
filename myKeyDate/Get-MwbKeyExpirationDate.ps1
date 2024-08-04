269795f6e1a6bba9a03c73652432e55990d64e07
Matti Pedersen <mrmattipants@gmail.com>
2024-08-03 20:43:31 -0500

    Add files via upload

---
+++ myKeyDate/Get-MwbKeyExpirationDate.ps1
@@ -0,0 +1,13 @@
+﻿$RegistryPath = 'HKEY_USERS\.DEFAULT\Software\Microsoft\MouseWithoutBorders'
+$Name         = 'myKeyDate'
+
+Write-Host "`nExisting `"myKeyDate`" Registry Value:"
+Write-Host "-----------------------------------"
+
+$RegistryKey = Get-ItemProperty -Path "REGISTRY::$($RegistryPath)" -Name $Name
+Write-Host "`nKey Generation Date (Base64):"
+Write-Host "$($RegistryKey.myKeyDate)" -ForegroundColor Green
+
+$DecodedString = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String("$($RegistryKey.myKeyDate)"))
+Write-Host "`nKey Generation Date:"
+Write-Host "$($DecodedString)" -ForegroundColor Green
---
+++ myKeyDate/Set-MwbKeyExpirationDate.ps1
@@ -0,0 +1,21 @@
+﻿$RegistryPath = 'HKEY_USERS\.DEFAULT\Software\Microsoft\MouseWithoutBorders'
+$Name         = 'myKeyDate'
+
+$Date = get-date -format "M-d-yyyy"
+#$Date = "4/4/2024"
+
+$StringBytes = [System.Text.Encoding]::Unicode.GetBytes($Date)
+$EncodedString = [Convert]::ToBase64String($StringBytes)
+
+New-ItemProperty -Path "REGISTRY::$($RegistryPath)" -Name $Name -Value $EncodedString -PropertyType String -Force | Out-Null
+
+Write-Host "`nNew `"myKeyDate`" Registry Value:"
+Write-Host "------------------------------"
+
+$RegistryKey = Get-ItemProperty -Path "REGISTRY::$($RegistryPath)" -Name $Name
+Write-Host "`nmyKeyDate (Encoded):"
+Write-Host "$($RegistryKey.myKeyDate)" -ForegroundColor Green
+
+$DecodedString = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String("$($RegistryKey.myKeyDate)"))
+Write-Host "`nmyKeyDate (Decoded):"
+Write-Host "$($DecodedString)" -ForegroundColor Green
